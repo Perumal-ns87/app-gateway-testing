c1a13cc16c4cdd3f0da189e0e9123c45c5d73aad
Perumal Natarajan <perumal.ns87@hotmail.com>
2025-02-21 15:26:31 +0530

    Added Private endpoint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World, Here I can use private endpoint for accessing the application'
 
 if __name__ == '__main__':
     app.run(debug=True)
